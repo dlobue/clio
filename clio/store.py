8db4c88fbc1f62daffbf7da0219169a3cfc76975
Dominic LoBue <dominic.lobue@gmail.com>
2011-06-02 12:41:31 -0700

    refactored to take advantage of the json attribute on flask response objects.

--- clio/store.py
+++ clio/store.py
@@ -8,15 +8,22 @@ app.config.from_object('clio.settings')
 try:
     app.config.from_envvar('CLIO_SETTINGS')
 except RuntimeError:
+    app.logger.debug("couldn't load settings from file listed in envvar")
     pass
 
 
 @app.route("/store/<host>/<sourcetype>/<float:timestamp>", methods=['PUT'])
 def store(host, sourcetype, timestamp):
-    db = pymongo.Connection(app.config['MONGO_HOSTS'], app.config['MONGO_PORT']).clio
-    data = request.data
     timestamp = datetime.utcfromtimestamp(timestamp)
+    if request.json is not None:
+        data = request.json
+    else:
+        app.logger.debug("data was in raw format")
+        data = request.data
+
     app.logger.info("host: %s, sourcetype: %s, timestamp: %s" % (host, sourcetype, timestamp))
+
+    db = pymongo.Connection(app.config['MONGO_HOSTS'], app.config['MONGO_PORT']).clio
     coll = db['_%i' % timestamp.year]['_{0:0>2}'.format(timestamp.month)][sourcetype]
     coll.update({
         'ts': timestamp,
@@ -28,7 +35,8 @@ def store(host, sourcetype, timestamp):
         upsert=True,
         safe=True)
 
-    return 'done'
+    return ''
+
 
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], port=app.config['PORT'])
