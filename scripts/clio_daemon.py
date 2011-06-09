18091efef351ecddb1d29ee7d01d0a7fb567a7b7
Dominic LoBue <dominic.lobue@gmail.com>
2011-06-08 17:31:12 -0700

    output flask logging into simpledaemon's log file.

--- scripts/clio_daemon.py
+++ scripts/clio_daemon.py
@@ -1,4 +1,6 @@
 
+import logging
+
 import simpledaemon
 
 class clio_daemon(simpledaemon.Daemon):
@@ -8,6 +10,10 @@ class clio_daemon(simpledaemon.Daemon):
     def run(self):
         import eventlet
         from clio.store import app
+        logger = logging.getLogger()
+        if logger.handlers:
+            [app.logger.addHandler(h) for h in logger.handlers]
+        app.logger.setLevel(logger.level)
         eventlet.serve(eventlet.listen((app.config['HOST'], app.config['PORT']), backlog=2048), app)
 
 if __name__ == '__main__':
