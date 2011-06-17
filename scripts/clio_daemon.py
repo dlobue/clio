2b8ef76e61e186b59a1c6837eb7ff0e6ab64b3e6
Dominic LoBue <dominic.lobue@gmail.com>
2011-06-17 08:04:32 -0700

    fix the clio_daemon to properly use the eventlet wsgi server, and to look for its config in /etc.

--- scripts/clio_daemon.py
+++ scripts/clio_daemon.py
@@ -2,13 +2,14 @@
 import simpledaemon
 
 class clio_daemon(simpledaemon.Daemon):
-    default_conf = 'clio_daemon.conf'
+    default_conf = '/etc/clio_daemon.conf'
     section = 'clio'
 
     def run(self):
         import eventlet
+        from eventlet import wsgi
         from clio.store import app
-        eventlet.serve(eventlet.listen((app.config['HOST'], app.config['PORT']), backlog=2048), app)
+        wsgi.server(eventlet.listen((app.config['HOST'], app.config['PORT']), backlog=2048), app)
 
 if __name__ == '__main__':
     clio_daemon().main()
