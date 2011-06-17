3daa15b0ccb3fc4891daf55724cbeaa705f923e5
Dominic LoBue <dominic.lobue@gmail.com>
2011-06-17 07:19:02 -0700

    Revert "output flask logging into simpledaemon's log file."
    This is completely superfluous - logging does this already
    automatically.
    
    This reverts commit 18091efef351ecddb1d29ee7d01d0a7fb567a7b7.

--- scripts/clio_daemon.py
+++ scripts/clio_daemon.py
@@ -1,6 +1,4 @@
 
-import logging
-
 import simpledaemon
 
 class clio_daemon(simpledaemon.Daemon):
@@ -10,10 +8,6 @@ class clio_daemon(simpledaemon.Daemon):
     def run(self):
         import eventlet
         from clio.store import app
-        logger = logging.getLogger()
-        if logger.handlers:
-            [app.logger.addHandler(h) for h in logger.handlers]
-        app.logger.setLevel(logger.level)
         eventlet.serve(eventlet.listen((app.config['HOST'], app.config['PORT']), backlog=2048), app)
 
 if __name__ == '__main__':
